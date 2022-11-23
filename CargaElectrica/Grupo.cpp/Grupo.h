3faba81f2b946cf92e0602239e81a5d771f1bce9
ocs1234 <72236120+ocs1234@users.noreply.github.com>
2022-11-23 08:32:59 -0600

    Update Grupo.h

--- CargaElectrica/Grupo.cpp/Grupo.h
+++ CargaElectrica/Grupo.cpp/Grupo.h
@@ -2,25 +2,29 @@
 // Created by Oscar Correa on 21/11/22.
 //
 
-# include "Grupo.h"
-using namespace std;
-Grupo::Grupo(string nom, float p , int  c) {
-    nombre=nom;
-    cargaGrupo=c;
-    PkGrupo=p;
-}
-Grupo::Grupo() {
-    nombre="Hugo";
-    cargaGrupo=0;
-    PkGrupo=0;
-}
+#include <string>
 
-string Grupo::GetN() {
-    return nombre;
-}
-float Grupo::Getpk() {
-    return PkGrupo;
-}
-int Grupo::GetCar() {
-    return cargaGrupo;
-}
+#ifndef PRACTICACARGAS_GRUPO_H
+#define PRACTICACARGAS_GRUPO_H
+
+using namespace std;
+
+class Grupo {
+
+public:
+    Grupo(string ,float ,int );//Constructor
+    Grupo();
+    string GetN();
+    float Getpk();
+    int GetCar();
+
+
+private:
+    string nombre;
+    int cargaGrupo;
+    float PkGrupo;
+
+};
+
+
+#endif //PRACTICACARGAS_GRUPO_H
